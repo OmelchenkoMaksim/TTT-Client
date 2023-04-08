4a234990f886f7aa1d9839944cfe587b63829c6f
Maksim Omelchenko <apostol_maxim@mail.ru>
2023-04-08 16:12:15 +0300

    first commit

---
+++ app/src/main/java/com/plcoding/onlinetictactoe/data/GameField.kt
@@ -0,0 +1,19 @@
+package com.plcoding.onlinetictactoe.data
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GameField(val playground: Array<Array<Char?>>) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as GameField
+
+        return playground.contentDeepEquals(other.playground)
+    }
+
+    override fun hashCode(): Int {
+        return playground.contentDeepHashCode()
+    }
+}
